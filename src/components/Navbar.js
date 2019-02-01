89e03088e6ac08635e42776b8008c7d84a046cb1
goodplanner <imtommypark@gmail.com>
2019-02-01 22:46:30 +0900

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ const Navbar = class extends React.Component {
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          <img src={logo} alt="GoodPlanner" style={{ width: '88px' }} />
         </Link>
         {/* Hamburger menu */}
         <div className="navbar-burger burger" data-target="navMenu">
@@ -49,7 +49,7 @@ const Navbar = class extends React.Component {
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
+        {/* <Link className="navbar-item" to="/products">
           Products
         </Link>
         <Link className="navbar-item" to="/contact">
@@ -57,7 +57,7 @@ const Navbar = class extends React.Component {
         </Link>
         <Link className="navbar-item" to="/contact/examples">
           Form Examples
-        </Link>
+        </Link> */}
       </div>
       <div className="navbar-end has-text-centered">
         <a
